e2fb4ea80190a2ada4ff96bcf8f98da1e749c81d
Izzana Barbosa Martins <91554353+izzana@users.noreply.github.com>
2022-05-03 09:35:35 -0300

    Trex stage 0

---
+++ Trex_Stage_0-main/Trex_Stage_0-main/sketch.js
@@ -0,0 +1,33 @@
+
+var trex ,trex_running, ground;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  //crie um sprite de trex
+ 
+
+  //crie um sprite ground (solo)
+
+
+  //adicione dimensão e posição ao trex
+}
+
+function draw(){
+  background("white");
+  
+  //condicional
+
+
+  //adicionar gravidade
+
+
+  //impedir que o trex caia
+
+
+  //função para desenhar os sprites
+}
